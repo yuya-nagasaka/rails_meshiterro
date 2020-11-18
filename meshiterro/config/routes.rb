99f47d1e39c748405bed955d8ef7c93fef051c9f
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2020-11-18 23:11:45 +0000

    いいね機能実装

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
  devise_for :users
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ resources :post_images, only: [:new, :create, :index, :show, :destroy]do
+   resource :favorites, only: [:create, :destroy]
+   resources :post_comments, only: [:create, :destroy]
+ end
 end
--- meshiterro/db/schema.rb
+++ meshiterro/db/schema.rb
@@ -10,26 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201117060708) do
+ActiveRecord::Schema.define(version: 2020_11_18_220715) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
-    t.text     "shop_name"
-    t.string   "image_id"
-    t.text     "caption"
-    t.integer  "user_id"
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
